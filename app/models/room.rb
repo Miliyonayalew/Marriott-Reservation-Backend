4f48c2ad9e688304d647e8e3a767f39b11a76c9c
Miliyonayalew <miliayalew@gmail.com>
2023-02-25 00:43:50 +0300

    Add assosication and validation for booking

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,8 +2,8 @@ class Room < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings, dependent: :destroy
 
-  validates :name, :price, :description, :image, :type, presence: true
-  validates :name, :image, :type, length: { maximum: 100 }
+  validates :name, :price, :description, :image, :room_type, presence: true
+  validates :name, :image, :room_type, length: { maximum: 100 }
   validates :description, length: { maximum: 400 }
-  validate :price, numericality: { greater_than_or_equal_to: 0 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
 end
