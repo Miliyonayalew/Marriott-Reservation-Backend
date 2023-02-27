34a77c7051eef07b3aff710e2782d66b7c8312cb
Miliyonayalew <miliayalew@gmail.com>
2023-02-28 02:39:31 +0300

    Modify devise dispatch request

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -313,10 +313,10 @@ Devise.setup do |config|
   config.jwt do |jwt|
     jwt.secret = Rails.application.credentials.fetch(:secret_key_base)
     jwt.dispatch_requests = [
-      ['POST', %r{^/users/sign_in$}]
+      ['POST', %r{^/api/v1/users/sign_in$}]
     ]
     jwt.revocation_requests = [
-      ['DELETE', %r{^/users/sign_out}]
+      ['DELETE', %r{^/api/v1/users/sign_out}]
     ]
     jwt.expiration_time = 120.minutes.to_i
   end
