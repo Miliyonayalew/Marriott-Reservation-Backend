09a6a14d41db6e33cdf71d5332f3dd4bcacda01a
Hans Zizold <hzizold@hotmail.com>
2023-02-26 20:07:01 -0400

    API Authentication with JWT

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  respond_to :json
+
+  def create
+    user = User.new(sign_up_params)
+
+    if user.save
+      render json: { user: user, message: 'Sign up success', is_success: true }, status: :ok
+    else
+      render json: { message: 'Sign up failed', is_success: false }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
