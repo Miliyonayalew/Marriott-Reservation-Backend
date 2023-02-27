21b0dca5783da6255fb265b9808bee486701074e
Miliyonayalew <miliayalew@gmail.com>
2023-02-28 02:36:37 +0300

    Fix offenses in the model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
+
+  has_many :bookings, dependent: :destroy
+  has_many :rooms, through: :bookings, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 40 }
 end
