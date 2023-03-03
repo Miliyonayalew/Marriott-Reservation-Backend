58955d074fbd09045c912fec41e19796edb3a004
Hans Zizold <hzizold@hotmail.com>
2023-03-03 16:01:05 -0400

    Update bookings_controller.rb

--- app/controllers/api/v1/bookings_controller.rb
+++ app/controllers/api/v1/bookings_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::BookingsController < ApplicationController
-  before_action :authenticate_user!, only: %i[create update destroy]
+  before_action :authenticate_user!
 
   def index
     render json: current_user.bookings.all
