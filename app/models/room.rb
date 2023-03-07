151dd4b8a2d12c669308551171699226de0cd879
Hans Zizold <hzizold@hotmail.com>
2023-03-07 16:17:08 -0400

    Update room.rb
    The allowed length of the image field of room table was updated

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,7 +3,7 @@ class Room < ApplicationRecord
   has_many :users, through: :bookings, dependent: :destroy
 
   validates :name, :price, :description, :image, :room_type, presence: true
-  validates :name, :image, :room_type, length: { maximum: 100 }
+  validates :name, :image, :room_type, length: { maximum: 500 }
   validates :description, length: { maximum: 400 }
   validates :price, numericality: { greater_than_or_equal_to: 0 }
 end
